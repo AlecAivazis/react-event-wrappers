6926deed74da72cb6b4278e12b4c3df264906cbf
Monte Mishkin <montemishkin@gmail.com>
2015-08-20 12:30:46 -0700

    added `EventHandler` wrapper component. Fails due to same binding issue

---
+++ src/components/EventHandler/tests.js
@@ -0,0 +1,17 @@
+/**
+ * Unit tests for `EventHandler` Component.
+ */
+
+/* common react imports */
+import React, {addons} from 'react/addons'
+const TestUtils = addons.TestUtils
+/* local imports */
+import EventHandler from './index'
+
+
+describe('EventHandler', function() {
+    it('should be tested')
+})
+
+
+// end of file
--- src/index.js
+++ src/index.js
@@ -7,11 +7,13 @@
 import 'babel/polyfill'
 
 /* local imports */
+import EventHandler from './components/EventHandler'
 import listenTo from './decorators/listenTo'
 import hoverable from './decorators/hoverable'
 
 // exports
 export default {
+    EventHandler: EventHandler,
     listenTo: listenTo,
     hoverable: hoverable,
 }
