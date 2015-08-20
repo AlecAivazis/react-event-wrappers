54df33f06ef9f0d8994fdf17a18724fba99116c7
Monte Mishkin <montemishkin@gmail.com>
2015-08-20 12:31:11 -0700

    `listenTo` now simply wraps in `EventHandler`

--- src/decorators/listenTo/index.js
+++ src/decorators/listenTo/index.js
@@ -1,23 +1,7 @@
 /* common react imports */
 import React from 'react/addons'
-/* misc third party imports */
-import {capitalize} from 'lodash'
-
-
-/**
- * Find the first decendant which is not an event-wrapper.
- * @param {ReactElement} element - The react element whose decendants you wish
- * to check.
- **/
-function find_true_child(element) {
-    // if wrapped component has not been flagged
-    if (!element._is_react_event_wrapper) {
-        // it is the true child
-        return element
-    }
-    // otherwise continue down the chain
-    return find_true_child(element.props.children)
-}
+/* local imports */
+import EventHandler from '../../components/EventHandler'
 
 
 /**
@@ -28,48 +12,20 @@ function find_true_child(element) {
 function listenTo(...args) {
     // use args as list of events
     let events = args
-    // if user passes single argument that is an array
+    // unless user passes single argument that is an array
     if (args.length === 1 && args[0].constructor === Array) {
-        // use it as list of events instead
+        // in which case we use that single arg as list of events instead
         events = args[0]
     }
-
-    // e.g. ["onMouseDown", "onMouseUp"]
-    const on_capitalized_events = events.map(event => 'on' + capitalize(event))
 
     // return decorator
     return function(Component) {
         // define wrapper component
         class WrapperComponent extends React.Component {
-            constructor(...constructor_args) {
-                // instantiate this
-                super(...constructor_args)
-                // add the wrapper flag
-                this._is_react_event_wrapper = true
-            }
-
             render() {
-                // wrapped component
-                const first_child = <Component {...this.props} />
-                // first decendant that isnt just one of our wrappers
-                const true_child = find_true_child(first_child)
-
-                // hash of listeners for the wrapping `div`
-                let listeners = {}
-                // iterate over the list of events
-                on_capitalized_events.forEach((event) => {
-                    // if true child has implemented a handler
-                    if (true_child.type.prototype[event]) {
-                        // store true child handler implementation
-                        listeners[event] = true_child.type.prototype[event]
-                            // and bind it to the true child instance
-                            .bind(true_child)
-                    }
-                })
-
-                return (<div {...listeners}>
-                    {first_child}
-                </div>)
+                return (<EventHandler events={events}>
+                    <Component {...this.props}/>
+                </EventHandler>)
             }
         }
 
--- src/decorators/listenTo/tests.js
+++ src/decorators/listenTo/tests.js
@@ -1,6 +1,5 @@
-/*
+/**
  * Unit tests for `listenTo` decorator.
- *
  */
 
 /* common react imports */
